9e80efb64ac80a452e8ded499d87ec2e1bf86336
Ayodeji Idowu <ayodeji.idowu@flutterwavego.com>
2024-01-13 11:58:41 +0100

    added fall back for local development

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 export const env = {
-    BASE_URL: process.env.BASE_URL,
+    BASE_URL: process.env.BASE_URL || "http://localhost:3000/",
     PORT: process.env.PORT,
-    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING,
+    MONGODB_CONNECTION_STRING: process.env.MONGODB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/clubmanagement",
     TOKEN_SECRET_KEY: process.env.TOKEN_SECRET_KEY,
     EMAIL_ADDRESS_OWNER: process.env.EMAIL_ADDRESS_OWNER,
     EMAIL_ADDRESS_PW: process.env.EMAIL_ADDRESS_PW,
